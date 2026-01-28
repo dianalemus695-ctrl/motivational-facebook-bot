3cbab4b301d0dd9695f55e6de16944379f211269
dianalemus695-ctrl <dianalemus695@gmail.com>
2026-01-28 13:33:16 -0800

    Randomize message selection for Facebook post

--- post.py
+++ post.py
@@ -1,14 +1,23 @@
 import os
 import requests
+import random
 
 PAGE_ACCESS_TOKEN = os.getenv("PAGE_ACCESS_TOKEN")
 PAGE_ID = os.getenv("PAGE_ID")
 
-MESSAGE = "Hello! This is an automated Facebook post from my bot!"
+quotes = [
+    "🌟 Believe in yourself. You are unstoppable.",
+    "🔥 Success comes from never giving up.",
+    "💪 Work hard today for a better tomorrow.",
+    "🚀 Your dreams are valid. Go get them!",
+    "✨ Stay positive. Stay strong.",
+]
+
+message = random.choice(quotes)
 
 url = f"https://graph.facebook.com/{PAGE_ID}/feed"
 data = {
-    "message": MESSAGE,
+    "message": message,
     "access_token": PAGE_ACCESS_TOKEN
 }
 
